118a58d44dac58bee5f261f3c3b5837b2aec84ed
lvtung8864 <FTWD1801010@st.fasttrack.edu.vn>
2019-05-22 09:19:37 +0700

    Updates

---
+++ precache-manifest.511cf6fa394623e47c3b496f5d35afad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a822881ff89d487cb007ee4a1c10b41f",
+    "url": "/shop-demo/index.html"
+  },
+  {
+    "revision": "b8eea422ae9475fe848f",
+    "url": "/shop-demo/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4fda57ecb38fc1df69fb",
+    "url": "/shop-demo/static/css/main.52d3a19e.chunk.css"
+  },
+  {
+    "revision": "b8eea422ae9475fe848f",
+    "url": "/shop-demo/static/js/2.71f91ceb.chunk.js"
+  },
+  {
+    "revision": "4fda57ecb38fc1df69fb",
+    "url": "/shop-demo/static/js/main.ce1c4772.chunk.js"
+  },
+  {
+    "revision": "65edfb989192a734d42b",
+    "url": "/shop-demo/static/js/runtime~main.d4f1ca13.js"
+  },
+  {
+    "revision": "5cea4fc089e3f43af3ac85f5a3d5c219",
+    "url": "/shop-demo/static/media/img1.5cea4fc0.jpg"
+  }
+]);
